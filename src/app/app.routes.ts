9d07e04db2047e872388db5b57aa37f6da6030c6
buiquiquyet <112879066+buiquiquyet@users.noreply.github.com>
2024-04-09 15:22:31 +0700

    add: sidebar

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,8 @@
 import { Routes } from '@angular/router';
+import { HeaderComponent } from './header/header.component';
+import { FooterComponent } from './footer/footer.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+   { path:'headerComponent', component:HeaderComponent},
+   { path:'footerComponent', component:FooterComponent},
+];
